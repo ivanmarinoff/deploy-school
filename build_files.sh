6f9fb1cac0c2e5f0abdac5a67556c8f048fe0478
ivanmarinoff <marinoffivan@gmail.com>
2024-01-01 17:01:50 +0200

    Updated entrypoint-2

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,14 @@
-pip install -r requirements.txt
-python3.11 manage.py collectstatic --noinput
+#pip install -r requirements.txt
+#python3.11 manage.py collectstatic --noinput
+
+#!/bin/sh
+
+#echo "Running Database Migrations"
+#python manage.py makemigrations --noinput
+#python manage.py migrate --noinput
+
+#echo "Running app commands"
+#python manage.py createcachetable
+#python manage.py collectstatic --noinput
+#python manage.py runserver &
+#exec "$@"
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 
-#echo "Running Database Migrations"
-#python manage.py makemigrations --noinput
-#python manage.py migrate --noinput
-
 echo "Running app commands"
-#python manage.py createcachetable
 python manage.py collectstatic --noinput
 python manage.py runserver &
 exec "$@"
