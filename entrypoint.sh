61831cad1c962fcb2da295075e852510b07ef9b1
ivanmarinoff <marinoffivan@gmail.com>
2024-01-01 16:25:31 +0200

    Updated entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-echo "Running Database Migrations"
-python manage.py makemigrations
-python manage.py migrate
+#echo "Running Database Migrations"
+#python manage.py makemigrations --noinput
+#python manage.py migrate --noinput
 
 echo "Running app commands"
 #python manage.py createcachetable
-python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput &
 python manage.py runserver &
 exec "$@"
