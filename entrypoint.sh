c377db16adc81e9f3e3a5277c3002873dcf148cc
ivanmarinoff <marinoffivan@gmail.com>
2024-01-01 16:50:56 +0200

    Updated entrypoint-1

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,6 @@
 
 echo "Running app commands"
 #python manage.py createcachetable
-python manage.py collectstatic --noinput &
+python manage.py collectstatic --noinput
 python manage.py runserver &
 exec "$@"
