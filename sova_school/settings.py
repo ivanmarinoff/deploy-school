0fe8387ca3bba1aded718059defc89ec3ce6d3ec
ivanmarinoff <marinoffivan@gmail.com>
2024-01-02 10:52:31 +0200

    Update storage settings

--- sova_school/settings.py
+++ sova_school/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    "whitenoise.runserver_nostatic",
     "django.contrib.staticfiles",
 
     "corsheaders",
@@ -163,10 +164,9 @@ STORAGES = {
     },
 }
 
-
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
-if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
+if not DEBUG:  # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
     STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
     # Turn on WhiteNoise storage backend that takes care of compressing static files
