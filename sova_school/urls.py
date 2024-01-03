3dfb82c252ab876c81d59d029e504ce0b6dc3850
ivanmarinoff <marinoffivan@gmail.com>
2024-01-03 15:00:08 +0200

    Updated app urls.py

--- sova_school/urls.py
+++ sova_school/urls.py
@@ -10,13 +10,12 @@ from django.conf import settings
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('sova_school.web.urls')),
-    path('content/', include('sova_school.content.urls')),
-    path('users/', include('sova_school.users.urls')),
-    path('global_content/', include('sova_school.global_content.urls')),
-    path('', include('sova_school.content.urls')),
-    path('', include('sova_school.global_content.urls')),
-    path('', include('sova_school.users.urls')),
-    path('', include('sova_school.web.urls')),
+    # path('content/', include('sova_school.content.urls')),
+    # path('users/', include('sova_school.users.urls')),
+    # path('global_content/', include('sova_school.global_content.urls')),
+    path('api/content/', include('sova_school.content.urls')),
+    path('api/global_content/', include('sova_school.global_content.urls')),
+    path('api/users/', include('sova_school.users.urls')),
 ]
 
 if not settings.DEBUG:
