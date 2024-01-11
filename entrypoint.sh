19b1a2b4bc9dd40feaee7b18183c01893aa723c2
ivanmarinoff <marinoffivan@gmail.com>
2024-01-11 17:15:23 +0200

    Updated 3

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-
+set -x
 #echo "Install the dependencies"
 #pip install -r requirements.txt
 
 
 #echo "Running collectstatic commands"
 #python manage.py collectstatic --noinput &
-
+set -e
 echo "Running app commands"
 python manage.py runserver &
-set -e
+
 exec "$@"
