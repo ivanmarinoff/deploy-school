47cbc8ccdf388ef9d170115683fd533004006e35
ivanmarinoff <marinoffivan@gmail.com>
2024-01-11 16:40:40 +0200

    Updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-echo "Install the dependencies"
-pip install -r requirements.txt
+#echo "Install the dependencies"
+#pip install -r requirements.txt
 
 
-echo "Running Database Migrations"
-python manage.py createcachetable
-python manage.py makemigrations
-python manage.py migrate
+#echo "Running Database Migrations"
+#python manage.py createcachetable
+#python manage.py makemigrations
+#python manage.py migrate
 
 echo "Running app commands"
 python manage.py collectstatic --noinput
