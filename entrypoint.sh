c2e21a029d7dbfe810f2c047313be85f858e75ba
ivanmarinoff <marinoffivan@gmail.com>
2024-01-11 17:40:35 +0200

    Updated 4

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@ set -x
 #pip install -r requirements.txt
 
 
-#echo "Running collectstatic commands"
-#python manage.py collectstatic --noinput &
+echo "Running collectstatic commands"
+python manage.py collectstatic --noinput &
 
 echo "Running app commands"
 python manage.py runserver 0.0.0.0:8000
