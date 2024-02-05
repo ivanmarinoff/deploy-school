bbe42b722755e30d5b867163d10b976f7be9c5e6
ivanmarinoff <marinoffivan@gmail.com>
2024-02-05 11:20:21 +0200

    Updated entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,5 @@ echo "Running collectstatic commands"
 python manage.py collectstatic --noinput
 
 echo "Running app commands"
-python manage.py runserver 0.0.0.0:8000
-
+python manage.py runserver &
 exec "$@"
